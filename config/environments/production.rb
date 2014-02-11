4eda081fce978faa7d678e60c142668b9960058c
Thoko Khansa <toulayegb@gmail.com>
2014-02-11 00:59:32 -0500

    devise installation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Pinteresting1::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  # necessary for heroku
+  # set my own domain later
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
